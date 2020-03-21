f59614245ad4d65f1d7103a5483613ae43c60b11
gtg092x <mdrake@mediadrake.com>
2020-03-21 16:59:08 -0700

    added orbs

--- integration/deploy.sh
+++ integration/deploy.sh
@@ -1,8 +1,8 @@
-VERSION=${2:-$(git log --pretty=format:'%h' -n 1)}
+VERSION=${CIRCLE_SHA1:-$(git log --pretty=format:'%h' -n 1)}
 TARGET=${1}
 
 aws lambda update-function-code \
     --function-name $TARGET \
     --s3-bucket lambda-covid-resources \
     --s3-key builds/$VERSION.zip \
-     --region us-west-2
+    --region us-west-2
--- integration/package.sh
+++ integration/package.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cat .lambdaignore | xargs zip -9qyr /var/bundles/lambda.zip . -x
+echo "Created bundle at $(pwd)"
 cp -a . /var/dist/
