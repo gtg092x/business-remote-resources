acaac99e64b1d23b2b20b7381c372321a6696b13
gtg092x <mdrake@mediadrake.com>
2020-03-21 19:23:10 -0700

    added tweak to test ci

--- integration/package.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cat .lambdaignore | xargs zip -9qyr /var/bundles/lambda.zip . -x
-echo "Created bundle at $(pwd)"
-cp -a . /var/dist/
--- integration/push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-VERSION=${CIRCLE_SHA1:-$(git log --pretty=format:'%h' -n 1)}
-
-aws s3 cp ./bundles/lambda.zip s3://lambda-covid-resources/builds/$VERSION.zip \
-     --region us-west-2
-
