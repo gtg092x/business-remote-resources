ffd7616f8ce0e56241cea20bb62f2bf99eedbe75
gtg092x <mdrake@mediadrake.com>
2020-03-21 19:42:04 -0700

    use client

--- shared/fetcher.ts
+++ shared/fetcher.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-export const client = axios.create({
-  baseURL: 'http://localhost:3000'
-})
+export const client = axios.create({})
 
 export const fetcher = url => client(url)
   .then(response => response.data)
